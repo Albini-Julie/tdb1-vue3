359b7e7903edfc5d16f8dc61422a2b38e3a5a5cb
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-09-09 09:55:15 +0200

    liste-fetch

--- src/App.vue
+++ src/App.vue
@@ -23,11 +23,21 @@
           
         </router-link>
       </li>
+      <li>
+        
+      <router-link class="text-red-600 underline" to="/liste-fetch">
+          Liste-fetch
+          
+        </router-link>
+      
+      </li>
     </ul>
   </nav>
 
   <!-- Affiche les pages -->
+  <Suspense>
   <router-view class="m-2 border-2 p-2" />
+</Suspense>
 </template>
 
 <script setup lang="ts">
---
+++ src/pages/liste-fetch.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Card from "../components/card.vue";
+const res = await fetch("/maisons.json");
+const maisons = await res.json();
+console.log(res);
+</script>
+    
+<template>
+     <div class="p-2">
+        <h2 class="text-2xl font-bold mb-5">Page simple</h2>
+        <Card v-for="maison in maisons" v-bind="maison" :key="maison.nom"/>
+    </div>
+</template>
