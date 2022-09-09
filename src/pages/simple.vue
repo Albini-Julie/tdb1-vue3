99bcf77780771e69b5883eb6cc5f4850d2a19986
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-09-09 08:50:03 +0200

    v-bind

--- src/pages/simple.vue
+++ src/pages/simple.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
 import Card from "../components/card.vue";
+
+const maison1 = {
+  nom: "Maison de campagne",
+  prix: 2400,
+  image: "/public/Mask Group.jpg",
+  préféré: true,
+  adresse: "Grand boulevard",
+  bed: "2",
+  bath: "1",
+  size: "100m²",
+};
 </script>
 
 
 <template>
     <div class="p-2">
         <h2 class="text-2xl font-bold mb-5">Page simple</h2>
-        <Card prix=2400 nom="Maison moderne" image="/public/pexels-binyamin-mellish-106399 1.jpg" préféré=true adresse="rue du Sapin" bed="3" bath="2" size="150m²"/>
+        <Card :prix=2400 nom="Maison moderne" image="/public/pexels-binyamin-mellish-106399 1.jpg" :préféré=true adresse="rue du Sapin" bed="3" bath="2" size="150m²"/>
+        <Card v-bind="maison1"/>
     </div>
 </template>
