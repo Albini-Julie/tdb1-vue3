ddf81dee111adc85fc7f44b725a52fa6ec6a3944
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-09-09 08:33:49 +0200

    mise en place des props individuelles

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,11 +1,33 @@
+<script setup lang="ts" >
+import IconBed from "./icones/bed.vue";
+import IconBath from "./icones/bath.vue";
+import IconSize from "./icones/size.vue";
+import IconCoeur from "./icones/coeur.vue";
+
+const props = defineProps({
+  prix: Number,
+  nom: String,
+  image: String,
+  préféré: Boolean,
+  adresse: String,
+  bed: String,
+  bath: String,
+  size: String,
+});
+</script>
+
+
 <template>
     <figure class="bg-white">
+        <div class="flex flex-col">
         <img class="rounded-t-lg" :src="image" alt="image maison">
         <figcaption class="px-5 py-7 border-2 border-t-0 rounded-b-lg">
             <div class="flex flex-col gap-2">
-                <div class="flex flex-wrap gap-1 items-center">
+                <div class="flex justify-between">
+                    <div class="flex items-center">
                     <p class="text-indigo-500 text-2xl font-bold font-inter">${{prix}}</p>
                     <p class="text-gray-500 font-inter text-base">/month</p>
+                </div>
                     <div><IconCoeur class="ml-24" :class="{ 'fill-red-300': préféré }"></IconCoeur></div>
                 </div>
                 <h3 class="flex-center font-inter text-2xl font-semibold text-gray-900">{{nom}}</h3>
@@ -18,24 +40,9 @@
                 <div><IconSize class="inline-block align-top"></IconSize>{{size}}</div>
             </div>
         </figcaption>
+        </div>
     </figure>
 </template>
 
-<script setup >
-import IconBed   from "./icones/bath.vue"
-import IconBath  from "./icones/bath.vue" 
-import IconSize  from "./icones/size.vue"
-import IconCoeur from "./icones/coeur.vue"
 
-    defineProps:{
-        prix= Number,
-        nom= String,
-        image= String,
-        préféré= Boolean,
-        adresse= String,
-        bed= String,
-        bath= String,
-        size= String,
-    },
-</script>
 
--- src/pages/simple.vue
+++ src/pages/simple.vue
@@ -1,11 +1,11 @@
-<template>
-    <div class="p-2">
-        <h2>Page simple</h2>
-        <Card/>
-    </div>
-</template>
-
 <script setup lang="ts">
-import Bath from "../components/icones/bath.vue";
 import Card from "../components/card.vue";
 </script>
+
+
+<template>
+    <div class="p-2">
+        <h2 class="text-2xl font-bold mb-5">Page simple</h2>
+        <Card prix=2400 nom="Maison moderne" image="/public/pexels-binyamin-mellish-106399 1.jpg" préféré=true adresse="rue du Sapin" bed="3" bath="2" size="150m²"/>
+    </div>
+</template>
