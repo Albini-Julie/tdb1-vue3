6f89f0925dd64d1c40f0ee9b9549a29ab19addaa
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-09-20 18:36:12 +0200

    test supabase

--- src/App.vue
+++ src/App.vue
@@ -49,6 +49,15 @@
           </router-link>
         
         </li>
+
+        <li>
+        
+        <router-link class="text-red-600 underline" to="/list-supabase">
+            liste-supabase
+            
+          </router-link>
+        
+        </li>
     </ul>
   </nav>
 
