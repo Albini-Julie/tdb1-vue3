7a18bd72218f6fd07cd2475e8485391c10861a06
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-09-27 21:50:39 +0200

    mise en place du menu (enc bug) + esthéti du site

--- src/pages/quartier/indexQuartier.vue
+++ src/pages/quartier/indexQuartier.vue
@@ -9,9 +9,28 @@
         <h3 class="text-2xl">Liste des quartiers</h3>
         <ul>
           <li v-for="quartierObject in (data as any[])">
-            {{ quartierObject.libelle_Commune }} -
+            <DisclosureButton>{{ quartierObject.libelle_Commune }}
+            </DisclosureButton> -
             {{ quartierObject.libelle_Quartier }}
           </li>
         </ul>
+
+        <Disclosure
+            v-for="(listeQuartier, libelle_Commune) in groupBy(
+            data,
+            'libelle_Commune'
+            )" 
+            :key="libelle_Commune"
+        ></Disclosure>
+
+  
+  <DisclosurePanel>
+    <ul><li
+  v-for="quartierObject in listeQuartier"
+  :key="quartierObject.code_Quartier"
+></li>
+</ul>
+</DisclosurePanel>
+        
       </section>
     </template>
